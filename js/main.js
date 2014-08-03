285b131995883622582c029d870fe34f3b96138d
Jordi Pàmies <jordipamies2@gmail.com>
2014-08-03 13:38:35 +0100

    Vaciado del main js

--- js/main.js
+++ js/main.js
@@ -1,22 +1,5 @@
 
 
-// Variables para capturar la fecha del navegador
-var fecha = new Date();
-var numero = fecha.getDate();
-var dia = fecha.getDay();
-var mes = fecha.getMonth();
-var horas = fecha.getHours();
-var minutos = fecha.getMinutes();
-
-//Variables para capturar elementos
-var texto = document.getElementsByClassName('detalleEvento');
-var prog = document.getElementsByClassName('llistaVisible');
-var bot = document.getElementsByClassName('dia');
-
-// Arreglos para a mostrar meses y días en Catalán
-var meses = ["Gener", "Febrer", "Març", "Abril", "Maig", "Juny", "Juliol", "Agost", "Setembre", "Octubre" ,"Novembre", "Desembre"];
-var diasSemana = ["Diumenge", "Dilluns", "Dimarts", "Dimecres", "Dijous", "Divendres", "Dissabte"];
-
 //función para subir o bajar el tamaño de la fuente
 function resizer (multiplicador){
     for (var i = 0; i < texto.length; i++) {
@@ -31,24 +14,6 @@ function resizer (multiplicador){
         };
 };
 
-//mostrar el programa de un dia concreto
-function mostrar(b){
-    // resetearProg();
-    var elem = document.getElementById(b);
-    elem.style.display='block';
-
-};
-
-// //eliminar el programa de el dia anterior
-// function resetearProg(){
-//     for (var i = 0; i < prog.length; i++) {
-//         if (prog[i].className=="llistaVisible"){       //creo que no hace falta el [if]
-//             prog[i].className="llistaInvisible";
-//         }else{
-//             prog[i].className="llistaInvisible";
-//         }
-//     };
-// };
 
 function ivan () {
   document.getElementById('dia1').style.display='none'
@@ -60,62 +25,6 @@ function slideToggle(a){
   $(elem).slideToggle(400);
 };
 
-
-// //función para mostrar y esconder el menu
-// var num = 100; //numero de pixele antes de aplicar el script
-
-// $(window).bind('scroll', function () {
-//     if ($(window).scrollTop() > num) {
-//         $('#menu').addClass('menu');
-//     } else {
-//         $('#menu').removeClass('menu');
-
-//     }
-// });
-
-//función para animar el despliegue de botonera
-$(function(){
-  //clic en un enlace de la lista
-  $('ul li a').on('click',function(e){
-    //prevenir en comportamiento predeterminado del enlace
-    e.preventDefault();
-    //obtenemos el id del elemento en el que debemos posicionarnos
-    var strAncla=$(this).attr('href');
-    
-    //utilizamos body y html, ya que dependiendo del navegador uno u otro no funciona
-    $('body,html').stop(true,true).animate({
-      //realizamos la animacion hacia el ancla
-      scrollTop: $(strAncla).offset().top
-    },500);
-  });
-});
-
-
-
-
-
-
-
-
-
-
-var primera = '<div class="llamadaCalendario"><h3>Programa de festes <span>Estigues al dia dels events</span></h3></div><article class="salutaciones"><div class="header"><h2>Salutacions</h2></div><div class="imagen"></div><div class="footer"><a href="personalitats.html">Galeria de personalitats</a></div></article><article class="pubillas"><div class="header"><h2>Pubilles</h2></div><div class="caras"><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla1.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla2.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla3.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla4.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla5.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla6.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla7.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla8.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla9.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla10.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla11.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla12.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla13.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div><div class="cara"><a href="siminaAC.html" class="enlace"><div class="imagen"><img src="img/pubilles/pubilla14.jpg" height="180px" width="180px" alt=""></div><div class="nombre"><span>Simina<br>Alexandra Crisan</span></div></a></div></div><div class="footer"><a href="javascript:irPubillas()">Galeria de pubillas</a></div></article>'
-
-
-
-
-var segona = '<div class="llamadaCalendario"><h3>Programa de festes <span>Estigues al dia dels events</span></h3></div><div class="volverIncicio" onclick="irInicio()"><span>Tornar al Inici</span></div><article class="postRecojido"><div class="header"><div class="hero hero1"></div><div class="foto"><img src="img/pubilles/pubilla1.jpg" height="180px" width="180px" alt="" /></div><div class="nombre"><p>Simina Alexandra Crisan</p></div><div class="subNombre"><p>Penya Barcelonista d"Ulldecona</p></div></div><input type="checkbox" id="desplegarPost" /><label class="desplegarPost" for="desplegarPost"><div class="contenido" onclick="slideToggle(\'pubilla1\')"><p>Hola falduts i faldudes!</p><p>Ja tenim les festes d’Ulldecona aquí, i un any més, totes les pubilles estem preparades i, sobretot, molt nervioses, per a representar de la millor manera possible les diferents entitats del poble.</p><div id="pubilla1" style="display:none;"><p>Farem que estes festes siguin inoblidables i les gaudirem al màxim, començant per la nit del pregó i posant el punt i final amb els focs artificials.</p><br><p>Una d’estes pubilles sereé jo, Simina. Tinc disset anys, acabo de cursar1r de Batxillerat en la modalitat Artística a Tortosa i de cara al futur m’agradaria estudiar a la universitat de Belles Arts. Sóc de nacionalitat romanesa, però porto tretze anys vivint a Ulldecona, i em considero una falduda més del poble, ja que sempre m’he sentit molt a gust aquí.</p><br><p>Com a pubilla representaré a la Penya Barcelonista d’Ulldecona, i agraeixo a la entitat que m’hagin donat la oportunitat de viure les festes d’una manera diferent, ja que des de ben petita he participat als seus concursos de dibuix i, fins i tot, he guanyat alguns premis.</p><br><p>Durant estes festes, jo seré una de les protagonistes, per això us invito a tots a que vingueu al nostre poble i que tots junts poguéssim gaudir de les tardes de bous, les nits d’orquestra, la pujada al Castell i tots els altres actes que es faran durant tota la setmana.</p><br><p>Xaleu al màxim durant les Festes 2013!</p><div class="fotoGrande"><img src="img/pubilles/pubillaGran1.jpg" width="478" heigt="589" alt="" /></div></div><div class="footer"><div class="flechita"></div></div></label></article><article class="postRecojido"><div class="header"><div class="hero hero1"></div><div class="foto"><img src="img/pubilles/pubilla2.jpg" height="180px" width="180px" alt="" /></div><div class="nombre"><p>Simina Alexandra Crisan</p></div><div class="subNombre"><p>Penya Barcelonista d"Ulldecona</p></div></div><input type="checkbox" id="desplegarPost" /><label class="desplegarPost" for="desplegarPost"><div class="contenido" onclick="slideToggle(\'pubilla2\')"><p>Hola falduts i faldudes!</p><p>Ja tenim les festes d’Ulldecona aquí, i un any més, totes les pubilles estem preparades i, sobretot, molt nervioses, per a representar de la millor manera possible les diferents entitats del poble.</p><div id="pubilla2" style="display:none;"><p>Farem que estes festes siguin inoblidables i les gaudirem al màxim, començant per la nit del pregó i posant el punt i final amb els focs artificials.</p><br><p>Una d’estes pubilles sereé jo, Simina. Tinc disset anys, acabo de cursar1r de Batxillerat en la modalitat Artística a Tortosa i de cara al futur m’agradaria estudiar a la universitat de Belles Arts. Sóc de nacionalitat romanesa, però porto tretze anys vivint a Ulldecona, i em considero una falduda més del poble, ja que sempre m’he sentit molt a gust aquí.</p><br><p>Com a pubilla representaré a la Penya Barcelonista d’Ulldecona, i agraeixo a la entitat que m’hagin donat la oportunitat de viure les festes d’una manera diferent, ja que des de ben petita he participat als seus concursos de dibuix i, fins i tot, he guanyat alguns premis.</p><br><p>Durant estes festes, jo seré una de les protagonistes, per això us invito a tots a que vingueu al nostre poble i que tots junts poguéssim gaudir de les tardes de bous, les nits d’orquestra, la pujada al Castell i tots els altres actes que es faran durant tota la setmana.</p><br><p>Xaleu al màxim durant les Festes 2013!</p><div class="fotoGrande"><img src="img/pubilles/pubillaGran2.jpg" width="478" heigt="589" alt="" /></div></div><div class="footer"><div class="flechita"></div></div></label></article><div class="volverIncicio" onclick="irInicio()"><span>Tornar al Inici</span></div>'
-
-function irPubillas () {
-
-  document.getElementById('contenidoPrincipal').innerHTML=segona;
-
-};
-
-function irInicio () {
-  document.getElementById('contenidoPrincipal').innerHTML=primera;
-}
-
 //Facebook
 (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
@@ -124,23 +33,6 @@ function irInicio () {
   js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.0";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
-
-// // Twitter
-// !function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');
-
-
-
-// Script mobileMenu
-$(function(){
-$('.mobileMenu').mobileMenu({
-  combine: false,         
-  groupPageText: '',     
-  nested: true,        
-  prependTo: 'nav',      
-  switchWidth: 770,     
-  topOptionText: ''
-});
-})
 
 
 // HTML comprimido para las seciones Pubillas, Información y Colaboradores. 
