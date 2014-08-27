e6eedcb98104383625f2ec280c88c3407de7313f
Jordi Pàmies <jordipamies2@gmail.com>
2014-08-27 11:58:42 +0100

    Estableciendo punto de inicio

--- js/main.js
+++ js/main.js
@@ -196,8 +196,6 @@ function actionBotonera()
       diasEventos.fadeOut();
       $('#dia14').fadeIn();
     }
-<<<<<<< HEAD
-=======
     else if ($(this).attr('id')=='d15')
     {
       diasEventos.fadeOut();
@@ -208,7 +206,6 @@ function actionBotonera()
       diasEventos.fadeOut();
       $('#dia16').fadeIn();
     }
->>>>>>> FETCH_HEAD
     else{console.log('la botonera esta fallando')}
   });
 };
@@ -283,10 +280,6 @@ preloadImg('nadar', 'http://ulldecona.herobo.com/image/fqu/eventos/nadar.jpg');
 preloadImg('nadar', 'http://ulldecona.herobo.com/image/fqu/eventos/nadar.jpg');
 preloadImg('chimchim', 'http://ulldecona.herobo.com/image/fqu/eventos/chimchim.jpg');
 preloadImg('chimchim2', 'http://ulldecona.herobo.com/image/fqu/eventos/chimchim2.jpg');
-<<<<<<< HEAD
-preloadImg('arte', 'http://ulldecona.herobo.com/image/fqu/eventos/arte.jpg');
-=======
->>>>>>> FETCH_HEAD
 
 var container = document.getElementById('imgContainer');
 container.style.display = 'block';
