12bf0b3de60ca87b2059738cc8a074e95e3b1595
Ivan Bardon <ivanbardon@gmail.com>
2014-09-02 07:17:29 +0200

    facebook was removed

--- js/main.js
+++ js/main.js
@@ -1101,13 +1101,3 @@ function plegar()
   $('div.evento .desplegado').slideUp();
   $('div.evento .plegado').fadeIn()
 };
-
-// //Facebook
-// (function(d, s, id) {
-//   var js, fjs = d.getElementsByTagName(s)[0];
-//   if (d.getElementById(id)) return;
-//   js = d.createElement(s); js.id = id;
-//   js.src = "//connect.facebook.net/es_ES/sdk.js#xfbml=1&version=v2.0";
-//   fjs.parentNode.insertBefore(js, fjs);
-// }(document, 'script', 'facebook-jssdk'));
-
